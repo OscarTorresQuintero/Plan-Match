ceea026251807521057ed16b3fff2bd385b27dce
felipecarrillo-eng <pipecrrll95@gmail.com>
2025-12-04 20:57:54 -0500

    Update ImageGallery.jsx

--- src/components/placeDetail/ImageGallery.jsx
+++ src/components/placeDetail/ImageGallery.jsx
@@ -1,1 +1,15 @@
+import React from "react";
 
+const ImageGallery = ({ imagen }) => {
+  return (
+    <div className="w-full">
+      <img
+        src={imagen}
+        alt="Foto del lugar"
+        className="w-full h-72 object-cover rounded-xl shadow"
+      />
+    </div>
+  );
+};
+
+export default ImageGallery;
