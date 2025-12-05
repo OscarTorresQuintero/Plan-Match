dee1c6e581c7e664ade340c801f0389aebf3ed1a
felipecarrillo-eng <pipecrrll95@gmail.com>
2025-12-04 20:57:45 -0500

    Update PlacesList.jsx

--- src/components/placeDetail/PlacesList.jsx
+++ src/components/placeDetail/PlacesList.jsx
@@ -1,1 +1,15 @@
+import React from "react";
+import PlaceCard from "./PlaceCard";
+import { lugares } from "./data/lugares";
 
+const PlacesList = ({ onSelect }) => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
+      {lugares.map((lugar) => (
+        <PlaceCard key={lugar.id} lugar={lugar} onSelect={onSelect} />
+      ))}
+    </div>
+  );
+};
+
+export default PlacesList;
