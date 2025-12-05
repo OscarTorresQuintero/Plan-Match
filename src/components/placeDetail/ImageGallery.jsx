3f68e5939ea7c8dd61c27707a8e4dce51ff25513
felipecarrillo-eng <pipecrrll95@gmail.com>
2025-12-04 21:21:31 -0500

    Update ImageGallery.jsx

--- src/components/placeDetail/ImageGallery.jsx
+++ src/components/placeDetail/ImageGallery.jsx
@@ -1,1 +1,10 @@
+function ImageGallery({ imagen }) {
+  return (
+    <div className="mb-4">
+      <img src={imagen} alt="Lugar" className="rounded-lg w-full" />
+    </div>
+  );
+}
+
+export default ImageGallery;
 
