76d4d1e149ddef95035808911fb46a4cc4f9cd9c
felipecarrillo-eng <pipecrrll95@gmail.com>
2025-12-04 21:21:20 -0500

    Update InfoSection.jsx

--- src/components/placeDetail/InfoSection.jsx
+++ src/components/placeDetail/InfoSection.jsx
@@ -1,1 +1,14 @@
+function InfoSection({ descripcion, rating, precio, horario, plan }) {
+  return (
+    <div className="mt-4">
+      <p className="mb-2"><strong>Descripción:</strong> {descripcion}</p>
+      <p className="mb-2"><strong>Rating:</strong> ⭐ {rating}/5</p>
+      <p className="mb-2"><strong>Precio:</strong> {precio} COP</p>
+      <p className="mb-2"><strong>Horario:</strong> {horario}</p>
+      <p className="mb-2"><strong>Tipo de plan:</strong> {plan}</p>
+    </div>
+  );
+}
+
+export default InfoSection;
 
