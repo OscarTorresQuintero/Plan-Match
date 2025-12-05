8ce06a38a90b75b00c8682fe2a4086a9f6f8d6ac
felipecarrillo-eng <pipecrrll95@gmail.com>
2025-12-04 20:58:38 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from "react";
+import PlacesList from "./PlacesList";
+import PlaceDetail from "./placeDetail/PlaceDetail";
 
+function App() {
+  const [selectedPlace, setSelectedPlace] = useState(null);
 
-useState
+  return (
+    <div className="max-w-5xl mx-auto">
+      {!selectedPlace ? (
+        <PlacesList onSelect={setSelectedPlace} />
+      ) : (
+        <PlaceDetail lugar={selectedPlace} onBack={() => setSelectedPlace(null)} />
+      )}
+    </div>
+  );
+}
+
+export default App;
