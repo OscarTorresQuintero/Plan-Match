72c98ba5d1aa6d9ade1cf274f28e62809ad387f1
felipecarrillo-eng <pipecrrll95@gmail.com>
2025-12-04 21:24:09 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
 import PlacesList from "./components/PlacesList";
 import PlaceDetail from "./components/placeDetail/PlaceDetail";
-import lugares from "./data/lugares";
+
 
 function App() {
   const [selectedPlace, setSelectedPlace] = useState(null);
