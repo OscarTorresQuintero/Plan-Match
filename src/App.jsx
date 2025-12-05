33bc6581f9a0fbb173b2c1302f945294b3ca57c5
felipecarrillo-eng <pipecrrll95@gmail.com>
2025-12-04 21:26:13 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
+import React, { useState } from "react";
 import PlacesList from "./components/PlacesList";
 import PlaceDetail from "./components/placeDetail/PlaceDetail";
-
+import { lugares } from "./data/lugares";
 
 function App() {
   const [selectedPlace, setSelectedPlace] = useState(null);
 
   return (
     <div className="p-4">
-      {selectedPlace ? (
-        <PlaceDetail
-          place={selectedPlace}
-          onBack={() => setSelectedPlace(null)}
-        />
+      {!selectedPlace ? (
+        <PlacesList onSelect={setSelectedPlace} />
       ) : (
-        <PlacesList places={lugares} onSelect={setSelectedPlace} />
+        <PlaceDetail place={selectedPlace} onBack={() => setSelectedPlace(null)} />
       )}
     </div>
   );
